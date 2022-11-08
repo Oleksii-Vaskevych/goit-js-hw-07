f200d0aa86b5f8cc508bd6426b5ba2f875defa28
Oleksii Vaskevych <alexeyffi@ukr.net>
2022-11-08 08:55:50 +0200

    add task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const galleryBigImageUrl = event.target.dataset.source;
+  // console.log(galleryBigImageUrl);
+
+  const instance = basicLightbox.create(`
+    <img src="${galleryBigImageUrl}" width="800" height="600">
+`);
+
+  instance.show();
+}
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` 
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+  `;
+    })
+    .join("");
+}
