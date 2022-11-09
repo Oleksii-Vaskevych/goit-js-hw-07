76186d37b31dfbcb252c76cac99647ca92dec1dc
Oleksii Vaskevych <alexeyffi@ukr.net>
2022-11-09 14:47:40 +0200

    fix task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -29,17 +29,15 @@ function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return ` 
-    <div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img
+          <a class="gallery__link" href="${original}">
+          <img
           class="gallery__image"
           src="${preview}"
           data-source="${original}"
           alt="${description}"
         />
       </a>
-    </div>
-  `;
+      `;
     })
     .join("");
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -17,12 +17,10 @@ function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return ` 
-    <div class="gallery__item">
         <a class="gallery__item" href="${original}">
         <img class="gallery__image" src="${preview}" alt="${description}" />
         </a>
-    </div>
-  `;
+      `;
     })
     .join("");
 }
